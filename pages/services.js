bac0357c21c459a0e10987e57eb6e77526903ca2
mourad <chabour.mourad87@gmail.com>
2019-12-18 20:19:55 +0100

    global middleware component

---
+++ pages/services.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Services extends Component {
+    render() {
+        return (
+            <>
+                <h1>Page services</h1>
+            </>
+        );
+    }
+}
+
+export default Services;
